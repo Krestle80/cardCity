39b269da35906bd5729032f5c38fd745f27e244f
Kronk <kylec0217@gmail.com>
2022-01-28 14:22:03 -0500

    can now place units on board, select them to see their movement range, and move them by clicking a tile within that range

--- script.js
+++ script.js
@@ -7,14 +7,45 @@ var canvasPosition = canvas.getBoundingClientRect();
 let hand = []
 let selectedCard = null
 let handPosition = null
+let playerGold = 0
+let playerGpt = 1
+let playerTech = 0
+let playerTpt = 1
+let maniplePt = -1
+let nextTurnHitBox = {
+    x: canvas.width/1.1,
+    y: canvas.height/100,
+    width:100,
+    height: 40
+}
+//global variable to hold wether or not a unit has been clicked on
+
+// var to hold the tile array position of currently selected unit
+let selectedUnit = {
+    selected: false,
+    position: null,
+    type: null
+}
+
 let sample = new Image( canvas.width/10, canvas.height/3.4);
 sample.src = 'trialCard.png'
-drawTopBottom = () => {
+
+let manipleCard = new Image()
+manipleCard.src = 'maniple.png'
+
+drawDisplay = () => {
+    //bars for top and bottom
     ctx.fillStyle = '#84a8db'
     ctx.fillRect(0,0, canvas.width, canvas.height/15)
     ctx.fillRect(0, canvas.height/1.5, canvas.width, canvas.height/2)
     ctx.fillStyle = "red"
     ctx.fillRect(canvas.width/1.1, canvas.height/100, 100,40)
+    ctx.fillStyle = "black"
+    ctx.font = "30px Arial"
+    ctx.fillText(playerGold, canvas.width/30, canvas.height/21)
+    ctx.fillText("(" + playerGpt + ")+", canvas.width/20, canvas.height/21)
+    ctx.fillText(playerTech, canvas.width/11, canvas.height/21)
+    ctx.fillText("(" + playerTpt + ")+", canvas.width/9.3, canvas.height/21)
 }
 
 const mouse = {
@@ -41,13 +72,16 @@ canvas.addEventListener('mousemove', (e) => {
 let tileArray = []
 
 class Tile {
-    constructor(x,y){
+    constructor(x,y, position){
         this.x = x;
         this.y = y;
         this.width = canvas.width/10
         this.height = canvas.height/10
         this.building = null 
         this.unit = null
+        this.highlighted = false
+        //position in tile array
+        this.position = position
     }
     update(){
         if(collison(this, mouse) && mouse.clicked == true && selectedCard === "fort"  ){
@@ -55,11 +89,102 @@ class Tile {
             this.building = selectedCard
             hand.splice(handPosition, 1)
             selectedCard = null
+            playerGpt +=1
+            maniplePt += 1
         }
+
+        if(collison(this, mouse) && mouse.clicked == true && this.unit){
+            selectedUnit.type = this.unit
+            selectedUnit.selected = true
+            selectedUnit.position = this.position
+            if(!(this.position.toString().split('')[1] === "0" || this.position == 0)){
+                if(!(this.position.toString().split('')[1] === "1")){
+                    tileArray[this.position-2].highlighted = true
+                }
+                tileArray[this.position-1].highlighted = true
+            }
+            if(!(this.position.toString().split('')[1] === "9"|| this.position == 9)){
+                tileArray[this.position+1].highlighted = true
+                tileArray[this.position+2].highlighted = true
+            }
+            if(!(this.position <= 9)){
+                if(!(this.position < 20)){
+                    tileArray[this.position-20].highlighted = true
+                }
+                tileArray[this.position-10].highlighted = true
+                if(!(this.position.toString().split('')[1] === "9"|| this.position == 9)){
+                tileArray[this.position-9].highlighted = true
+                }
+            }
+            if(!(this.position.toString().split('')[1] === "0" || this.position == 0 || this.position <= 9)){
+                tileArray[this.position-11].highlighted = true
+            }
+            if(!(this.position >= 50)){
+                tileArray[this.position+10].highlighted = true
+                if(!(this.position >= 40)){
+                    tileArray[this.position+20].highlighted = true
+                }
+                if(!(this.position.toString().split('')[1] === "9"|| this.position == 9)){
+                tileArray[this.position+11].highlighted = true
+            }
+                if(!(this.position.toString().split('')[1] === "0" || this.position==0)){
+                    tileArray[this.position+9].highlighted = true
+                }
+            }
+            
+        }
+       
+        if(collison(this,mouse) && mouse.clicked == true && this.highlighted == true ){
+            if(!(this.unit)){
+            tileArray[selectedUnit.position].unit = null
+            this.unit = selectedUnit.type}
+            if(!(selectedUnit.position.toString().split('')[1] === "0" || selectedUnit.position == 0)){
+                if(!(selectedUnit.position.toString().split('')[1] === "1")){
+                    tileArray[selectedUnit.position-2].highlighted = false
+                }
+                tileArray[selectedUnit.position-1].highlighted = false
+            }
+            if(!(selectedUnit.position.toString().split('')[1] === "9"|| selectedUnit.position == 9)){
+                tileArray[selectedUnit.position+1].highlighted = false
+                tileArray[selectedUnit.position+2].highlighted = false
+            }
+            if(!(selectedUnit.position <= 9)){
+                if(!(selectedUnit.position < 20)){
+                    tileArray[selectedUnit.position-20].highlighted = false
+                }
+                tileArray[selectedUnit.position-10].highlighted = false
+                if(!(selectedUnit.position.toString().split('')[1] === "9"|| selectedUnit.position == 9)){
+                tileArray[selectedUnit.position-9].highlighted = false
+                }
+            }
+            if(!(selectedUnit.position.toString().split('')[1] === "0" || selectedUnit.position == 0 || selectedUnit.position <= 9)){
+                tileArray[selectedUnit.position-11].highlighted = false
+            }
+            if(!(selectedUnit.position >= 50)){
+                tileArray[selectedUnit.position+10].highlighted = false
+                if(!(selectedUnit.position >= 40)){
+                    tileArray[selectedUnit.position+20].highlighted = false
+                }
+                if(!(selectedUnit.position.toString().split('')[1] === "9"|| selectedUnit.position == 9)){
+                tileArray[selectedUnit.position+11].highlighted = false
+            }
+                if(!(selectedUnit.position.toString().split('')[1] === "0" || selectedUnit.position==0)){
+                    tileArray[selectedUnit.position+9].highlighted = false
+                }
+            }
+
+        }
+        if(collison(this, mouse) && mouse.clicked == true && selectedCard === "maniple"  ){
+            if(this.building || this.unit) return
+            this.unit = selectedCard
+            hand.splice(handPosition, 1)
+            selectedCard = null
+        }    
+
     }
     draw(){
         ctx.lineWidth = 5
-        if(collison(this, mouse)){
+        if(collison(this, mouse) || this.highlighted == true){
             ctx.strokeStyle = "yellow"
             ctx.strokeRect(this.x,this.y,this.width,this.height)
             ctx.strokeStyle = '#2762b5'
@@ -74,15 +199,22 @@ class Tile {
             ctx.fillRect(this.x, this.y, this.width, this.height)
             ctx.fillStyle = '#6789b9'
         }
+        if(this.unit == "maniple"){
+            ctx.fillStyle = "red"
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+            ctx.fillStyle = '#6789b9'
+        }
         // else{
         // ctx.strokeRect(this.x,this.y,this.width,this.height)}
     }
 }
 
 let gridMaker = () =>{
+    let position = 0
     for(let y = canvas.height/15; y < canvas.height/1.6; y += canvas.height/10){
         for(let x =0; x < canvas.width; x += canvas.width/10){
-            tileArray.push(new Tile(x,y))
+            tileArray.push(new Tile(x,y, position))
+            position ++
         }
     }
 }
@@ -115,12 +247,17 @@ class Card {
         this.selected = false
     }
     update(){
+                if(collison(this, mouse) && mouse.clicked === true && this.selected === true) {
+                    selectedCard = null
+                    this.selected = false       
+                }
+            
                 if(collison(this, mouse) && mouse.clicked === true){
                 if(this.selected == true) return
                 selectedCard = this.type
                 this.selected = true
-        }
-    }
+                }
+            }
     draw(x){
         this.x = x
         if(collison(this, mouse) || this.selected == true){
@@ -142,8 +279,10 @@ class Card {
         ctx.fillStyle = '#6789b9'
         ctx.stroke()
         ctx.fill()
-
-        ctx.drawImage(sample, x - canvas.width/250, this.y - canvas.height/120)
+        if (this.type === "fort") ctx.drawImage(sample, x - canvas.width/250, this.y - canvas.height/120)
+        if (this.type === "maniple"){
+            ctx.drawImage(manipleCard, x - canvas.width/250, this.y - canvas.height/120)
+        }
         ctx.fillStyle = 'white'
         ctx.strokeStyle = '#2762b5'
     }
@@ -163,12 +302,28 @@ let handHandler = () => {
 
 }
 
+
+let turnHandler = () =>{
+    if(collison(nextTurnHitBox, mouse) && mouse.clicked == true)
+    {
+        playerGold += playerGpt
+        playerTech += playerTpt
+        if (maniplePt >= 0 ){
+            for(let i = 0; i <= maniplePt ; i ++)
+            hand.push(new Card("maniple"))
+        } 
+    }
+}
 //main animation loop
 let animate = () =>{
     ctx.clearRect(0,0, canvas.width, canvas.height)
-    drawTopBottom()
+    turnHandler()
+    drawDisplay()
     drawTiles()
     handHandler()
+    if(mouse.clicked == true){
+        mouse.clicked= false
+    }
     requestAnimationFrame(animate);
 }
 
@@ -177,9 +332,10 @@ let startUp = () => {
     let starty = (Math.floor(Math.random()*6))*10
     let settlementTile = startx+starty
     tileArray[settlementTile].building = "settlement" 
-    for( i = 0; i < 9; i ++){
+    for( i = 0; i < 4; i ++){
         hand.push(new Card("fort"))
     }
+    hand.push(new Card("maniple"))
     animate()
 }
 startUp()
